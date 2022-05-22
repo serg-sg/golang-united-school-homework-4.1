9481d83b03ae77cf8d6d3d89f870c91d6565f9fd
Eronov Sergey <serg_eronov@live.ru>
2022-05-22 15:44:42 +0400

    Done

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,20 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+
+	a := []rune(input) // Сделаем копию input с преобразованием в rune
+
+	b := make([]rune, len(a)) // Создадим пустую rune для output
+
+	x := len(a) - 1 // Инициализируем переменную
+	//для обратного отсчёта индексов rune для возврата результата
+
+	// Перевернём rune
+	for _, value := range a {
+
+		b[x] = value
+		x--
+	}
+
+	return string(b) // Преобразуем rune в string
 }
